075342bfa3161e360602a523894f3028cfe4efaa
austinChappell <Austin.W.Chappell@gmail.com>
2018-02-12 12:22:42 -0600

    can add photos

--- routes/photo.js
+++ routes/photo.js
@@ -7,12 +7,34 @@ const Photo = require('../models/photo')
 const cloudinary = require('cloudinary')
 
 router.get('/', authRequired, (req, res) => {
-  res.render('admin/photo', { active: 'Photos' })
+  Photo.find({}, (err, photos) => {
+    if (err) {
+      res.status(500).send(err)
+    }
+    res.status(200).render('admin/photo', { active: 'Photos', photos })
+  })
 })
 
 router.post('/', authRequired, (req, res) => {
   console.log('BODY', req.body)
-  cloudinary.uploader.upload()
+  cloudinary.v2.uploader.upload(req.body.image, function(err, result) {
+    if (err) {
+      console.error('ERROR UPLOADING TO CLOUDINARY', err);
+    } else {
+      console.log('RESULT', result);
+      const image = {
+        title: req.body.title,
+        url: result.url
+      }
+      const photo = new Photo(image)
+      photo.save((err, newPhoto) => {
+        if (err) {
+          res.status(500).send(err)
+        }
+        res.status(200).send(newPhoto)
+      })
+    }
+  })
 })
 
 module.exports = router;
