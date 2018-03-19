d986d94c417e8a99e319ee20630a1e67333c134a
austinChappell <Austin.W.Chappell@gmail.com>
2018-03-19 07:21:07 -0500

    wip on mobile nav bar

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,17 @@
 const hamburger = document.querySelector('.hamburger');
+const hamburgerLines = document.querySelectorAll('.hamburger .line');
+const mobileNav = document.querySelector('.mobile-nav ul');
 const navbar = document.querySelector('.navbar');
 
 navbar.addEventListener('click', (evt) => {
+  const { link } = evt.target.dataset;
+  const page = document.getElementById(link);
+  if (page) {
+    page.scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+mobileNav.addEventListener('click', (evt) => {
   const { link } = evt.target.dataset;
   const page = document.getElementById(link);
   if (page) {
@@ -14,14 +24,43 @@ window.addEventListener('scroll', () => {
   if (y > 200) {
     navbar.style.backgroundColor = 'rgba(0,0,0,0.2)';
     navbar.style.transition = '500ms';
-    navbar.style.padding = '5px 160px';
+    navbar.style.padding = '5px 10%';
   } else {
     navbar.style.backgroundColor = 'rgba(0,0,0,0)';
     navbar.style.transition = '500ms';
-    navbar.style.padding = '30px 160px';
+    navbar.style.padding = '30px 10%';
   }
 });
 
 hamburger.addEventListener('click', (evt) => {
-  console.log('HAMBURGER CLICKED')
+  if (mobileNav.style.bottom != '0px') {
+    mobileNav.style.bottom = 0;
+    mobileNav.style.opacity = 1;
+    hamburgerLines[1].style.left = '200%';
+    hamburgerLines[1].style.opacity = 0;
+    hamburgerLines[0].style.transform = 'rotate(45deg)';
+    hamburgerLines[0].style.top = '25%';
+    hamburgerLines[2].style.transform = 'rotate(-45deg)';
+    hamburgerLines[2].style.bottom = '25%';
+  } else {
+    mobileNav.style.bottom = '100vh';
+    mobileNav.style.opacity = 0;
+    hamburgerLines[1].style.left = 0;
+    hamburgerLines[1].style.opacity = 1;
+    hamburgerLines[0].style.transform = 'rotate(0)';
+    hamburgerLines[0].style.top = 0;
+    hamburgerLines[2].style.transform = 'rotate(0)';
+    hamburgerLines[2].style.bottom = 0;
+  }
+})
+
+mobileNav.addEventListener('click', () => {
+  mobileNav.style.bottom = '100vh';
+  mobileNav.style.opacity = 0;
+  hamburgerLines[1].style.left = 0;
+  hamburgerLines[1].style.opacity = 1;
+  hamburgerLines[0].style.transform = 'rotate(0)';
+  hamburgerLines[0].style.top = 0;
+  hamburgerLines[2].style.transform = 'rotate(0)';
+  hamburgerLines[2].style.bottom = 0;
 })
