02eb5099892b93521304d60e150b6fa2aeb6ea92
austinChappell <Austin.W.Chappell@gmail.com>
2018-05-31 23:11:54 -0500

    show active video

--- public/scripts/video.js
+++ public/scripts/video.js
@@ -2,13 +2,21 @@ const videoPlayButtons = document.querySelectorAll('.video-list .play-pause');
 const videoPlayer = document.querySelector('.video-player');
 const videoModal = document.querySelector('.video-modal');
 const videoClose = document.querySelector('.video-close');
+const videoItems = document.querySelectorAll('.video-list li');
 
-videoPlayButtons.forEach((btn) => {
+videoPlayButtons.forEach((btn, index) => {
   btn.addEventListener('click', () => {
     const parent = btn.parentElement;
     const { url } = parent.dataset;
     videoModal.classList.remove('hide');
     videoPlayer.src = url;
+    videoItems.forEach((item, idx) => {
+      if (index === idx) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    });
   });
 });
 
